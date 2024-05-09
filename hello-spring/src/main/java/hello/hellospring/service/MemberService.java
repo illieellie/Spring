65d1b7da9e6f02a9ac704b14393a4d36d38b34e8
imellie <ssul4666@naver.com>
2024-05-09 18:26:34 +0900

    member-service-logic

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -4,6 +4,7 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
@@ -13,12 +14,37 @@ public class MemberService {
 
     // 회원 가입
     public Long join(Member member) {
-        // 같은 이름이 있는 중복 회원은 안 된다.
+        // 같은 이름이 있는 중복 회원은 X.
 
-        Optional<Member> result =  memberRepository.findByName(member.getName());
+        // null 이 반환될 가능성이 있으면 Optional로 한번 감싸서 반환 (if문을 사용할 필요가 없어짐)
+
+        // 아래와 같이 Optional 형을 바로 꺼내는 것을 권장하지 않음
+        // Optional<Member> result =  memberRepository.findByName(member.getName());
+
+        // 따라서 아래와 같이 작성
+        validateDuplicateMember(member); // 중복 회원 검증
 
         memberRepository.save(member);
         return member.getId();
 
     }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m->{
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                    // member에 값이 있으면 수행 : 예외 발생
+        });
+    }
+
+
+    public List<Member> findMembers() {
+        // 전체 회원 조회
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
 }
