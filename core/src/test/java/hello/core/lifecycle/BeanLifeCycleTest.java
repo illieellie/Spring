609c0805d1d1368e1c91c956c9bda3330ec451cb
imellie <ssul4666@naver.com>
2024-06-10 19:23:43 +0900

    bean-lifeCycle-practice

---
+++ core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,32 @@
+package hello.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest(){
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        // close 기능은 ApplicationContext 에서는 제공하지 않는다.
+        // 보다 하위 기능인 ConfigurableApplicationContext 까지 내려가야함
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+
+    }
+    @Configuration
+    static class LifeCycleConfig{
+        @Bean
+        public NetworkClient networkClient(){
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            // 객체 생성을 하고 set으로 url을 지정해줬기 때문에 connet 메소드에서는 url이 null
+            return networkClient;
+        }
+
+
+    }
+}
