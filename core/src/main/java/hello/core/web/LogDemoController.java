695db3c0767ddc1d629a6ae6985be09d40dfe1f4
imellie <ssul4666@naver.com>
2024-06-17 19:26:55 +0900

    scope-proxy

--- core/src/main/java/hello/core/web/LogDemoController.java
+++ core/src/main/java/hello/core/web/LogDemoController.java
@@ -12,18 +12,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequiredArgsConstructor
 public class LogDemoController {
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
     // 스프링 컨테이너가 뜨면서 의존관계 주입을 해야하는데 mylogger는 request scope이라 아직 고객 요청이 없어서 에러 => ObjectProvider 사용
-
+    // 위 코드를 줄이기 위해 프록시 사용 @Scope(value="request",proxyMode = ScopedProxyMode.TARGET_CLASS)
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request){
         String requestURL = request.getRequestURL().toString(); // 고객이 요청한 url을 받을 수 있음
-        MyLogger myLogger = myLoggerProvider.getObject(); // 주입 시점에 주입 받을 수 있음
+        //MyLogger myLogger = myLoggerProvider.getObject(); // 주입 시점에 주입 받을 수 있음
         myLogger.setRequestURL(requestURL);
         myLogger.log("controller test");
         logDemoService.logic("testId");
         return "OK";
     }
-
 }
